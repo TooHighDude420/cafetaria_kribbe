f8be3694d2e904af9f4f066f4c07e7c63fc42941
TooHighDude <nataro112@gmail.com>
2024-12-02 14:43:01 +0100

    02-12-2024
    Index gemaakt en ook een test include voor de style

---
+++ includes/navbar.inc.php
@@ -0,0 +1,8 @@
+<div class="p-10 max-w-full h-[10vh] bg-black flex items-center">
+    <div class="w-fit h-fit mx-8">
+        <img class="max-h-[80px] max-w-[200px]" src="img/navbar/logo.png" alt="kribbe_logo">
+    </div>
+    <div class="bg-white w-1/4 h-[5vh] mx-8"></div>
+    <div class="bg-white w-1/4 h-[5vh] mx-8"></div>
+    <div class="bg-white w-1/4 h-[5vh] mx-8"></div>
+</div>
