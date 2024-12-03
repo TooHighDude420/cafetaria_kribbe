38d4b24b82e7b96e38ebbdac80f10261fe8c8826
TooHighDude <nataro112@gmail.com>
2024-12-03 21:51:04 +0100

    Update menu.inc.php
    bon toegevoegd voor het testen van de javascript

--- includes/menu.inc.php
+++ includes/menu.inc.php
@@ -1,8 +1,13 @@
-<div class="flex justify-center mt-5">
+<div class="flex flex-col flex-wrap content-center gap-y-5 mt-5">
     <div class="flex block w-4/5 p-6 bg-[#F8F7F3] border border-gray-200 rounded-lg shadow">
         <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Frietje oorlog</h5>
             <p class="font-normal text-gray-700">Friet met pinda-saus en mayonaise</p>
+        </div>
+    </div>
+    <div class="flex block w-4/5 p-6 bg-[#F8F7F3] border border-gray-200 rounded-lg shadow">
+        <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Bon item 1</h5>
         </div>
     </div>
 </div>
