3afe9ef609b068a659b008c76da4230e77805ede
TooHighDude <nataro112@gmail.com>
2024-12-04 18:39:22 +0100

    Update menu.inc.php
    functie voor de verwijzing toegevoegd

--- includes/menu.inc.php
+++ includes/menu.inc.php
@@ -5,7 +5,7 @@
             <p class="font-normal text-gray-700">Friet met pinda-saus en mayonaise</p>
         </div>
     </div>
-    <div class="flex block w-4/5 p-6 bg-[#F8F7F3] border border-gray-200 rounded-lg shadow" id="cartcontainer">
+    <div onload="onLoad()" class="flex block w-4/5 p-6 bg-[#F8F7F3] border border-gray-200 rounded-lg shadow" id="cartcontainer">
         <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Bon item 1</h5>
         </div>
