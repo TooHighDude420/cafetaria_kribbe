3725630baefb299bbe7fce24fd7bc2b6baa77e31
TooHighDude <nataro112@gmail.com>
2024-12-04 23:18:26 +0100

    Update basket.js
    test code weg gehaald

--- javascript/menu/basket.js
+++ javascript/menu/basket.js
@@ -40,22 +40,3 @@ function removeFromCart(id, name, dis, cost){
     cart.splice(index, 1);
     elToDel.remove();
 }
-
-// function popCart(){
-//     for (var i = 0; i < cart.length; i++){
-//         var temp = Object.assign(document.createElement("div"), {className: "block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow"});
-//         var title = Object.assign(document.createElement("h5"), {className: "mb-2 text-2xl font-bold tracking-tight text-gray-900"});
-//         var discription = Object.assign(document.createElement("p"), {className: "font-normal text-gray-700"});
-//         var price = Object.assign(document.createElement("p"), {className: "font-normal text-gray-700"});
-
-//         temp.id = "cart-item-" + i;
-//         title.innerHTML = cart[i][0];
-//         discription.innerHTML = cart[i][1];
-//         price.innerHTML = "$" + cart[i][2];
-
-//         cartContainer.append(temp);
-//         temp.append(title);
-//         title.append(discription);
-//         discription.append(price);
-//     }
-// }
