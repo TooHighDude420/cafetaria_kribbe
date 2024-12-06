2eb20b13fe99c3a88b865f1c374653316884e701
TooHighDude <nataro112@gmail.com>
2024-12-06 09:36:57 +0100

    Update add.php
    voorbereiden voor het vullen van de database

--- php/add.php
+++ php/add.php
@@ -5,37 +5,52 @@ $data_bet = [];
 $i = 0;
 
 $csv = fopen("../csv/cafetaria_menu_filled.csv", "r");
-while (($data = fgetcsv($csv)) !== false){
-   $data_bet[$i] = $data;
-   $i++;
+while (($data = fgetcsv($csv)) !== false) {
+    $data_bet[$i] = $data;
+
+    $name = $data_bet[$i][0];
+    $beschrijving = $data_bet[$i][1];
+    $prijs = $data_bet[$i][2]; 
+    $cat = $data_bet[$i][3];
+
+    print "<pre>";
+    print "<p> menu item $i: </p>";
+    print_r($name);
+    print "<br>";
+    print_r($beschrijving);
+    print "<br>";
+    print_r($prijs);
+    print "</pre>";
+    
+
+    print "<pre>";
+    print "<p> cat van item: $i: </p>";
+    print_r($cat);
+    print "</pre>";
+
+    $sth = $conn->prepare("INSERT (`naam`, `beschrijving`, `prijs`, `categorie`, `image`) INTO `menu` VALUES (?, ?, ?, 2, null)");
+    $sth->bindParam("?", $name);
+    $sth->bindParam("?", $beschrijving);
+    $sth->bindParam("?", $prijs);
+    $sth->execute();
+
+    $i++;
 }
 
 print "<pre>";
-print"<p> menu items: </p>";
+print "<p> menu items: </p>";
 print_r($data_bet);
 print "</pre>";
 
-for ($i = 1; $i < $data_bet; $i++){
-    $name = $data_bet[$i][0];
-    print $name;
-    $beschrijving = $data_bet[$i][1];
-    print $beschrijving;
-    $prijs = $data_bet[$i][2];
-    print $prijs;
-    $cat = $data_bet[$i][3];
-    print $prijs;
-}
 
 
 try {
     // $title = $_POST['Title'];
     // $item = $_POST['Item'];
     // $price = $_POST['Price'];
-    // $sth = $conn->prepare("INSERT (`naam`, `beschrijving`, `prijs`, `categorie`, `image`) INTO `menu` VALUES (`kapsalon kip`, `ftiet, kipdoner, sla en kaas`, 15.50, 2, null)");
-    // $sth->execute();
     // $data_add = $conn->prepare(query: "INSERT INTO menu (Title, Item, Price) VALUES ($title, '$item', '$price')");
     // $data_add->execute();
     // header('Location:http://localhost/Tb_site/index.php?page=additem');
 } catch (PDOException $e) {
-    echo "Error: ". $e->getMessage();
+    echo "Error: " . $e->getMessage();
 }
